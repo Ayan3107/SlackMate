f8d81943d804ee9677ac0921f3eb5847827ee26e
Ayan Hitkari <ayanhitkari@gmail.com>
2026-09-15 21:18:02 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,54 @@
+require("dotenv").config();
+
+const axios =require("axios")
+const { App } = require("@slack/bolt");
+
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  appToken: process.env.SLACK_APP_TOKEN,
+  socketMode: true
+});
+
+app.command("/slackmate-ping", async ({ command, ack, respond }) => {
+  const start = Date.now();
+
+  await ack();
+
+  const latency = Date.now() - start;
+
+  await respond({
+    text: `Pong!\nLatency: ${latency}ms`
+  });
+});
+
+(async () => {
+  await app.start();
+  console.log("SlackMate is running!");
+})();
+
+app.command("/slackmate-help", async ({ ack, respond }) => {
+  await ack();
+
+  await respond({
+    text:
+`Available Commands:
+/slackmate-ping - Check bot latency
+/slackmate-help - Show available commands`
+  });
+});
+
+app.command("/slackmate-catfact", async ({ ack, respond }) => {
+  await ack();
+
+  try {
+    const response = await axios.get("https://catfact.ninja/fact");
+
+    await respond({
+      text: `Cat Fact:\n${response.data.fact}`
+    });
+  } catch (err) {
+    await respond({
+      text: "Failed to fetch a cat fact."
+    });
+  }
+});
